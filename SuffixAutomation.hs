a88bd05418c75800bf21c1e7c040966fb115a94f
klokik <0xb000@gmail.com>
2013-10-23 08:55:28 +0300

    O_o

--- SuffixAutomation.hs
+++ SuffixAutomation.hs
@@ -1,4 +1,11 @@
-module SuffixAutomation where
+module SuffixAutomation
+    (
+    SState(),
+    SuffixAutomation(),
+    empty,
+    fromList,
+    addItem
+    )where
 
 import qualified Data.Map as M
 import Prelude
@@ -16,14 +23,14 @@ data SuffixAutomation a = SuffixAutomation {    states :: M.Map Int (SState a)
                                             }   deriving (Show)
 
 
-emptyAut :: SuffixAutomation a
-emptyAut = SuffixAutomation {states = M.fromList [(0,(SState {len = 0, link = Nothing, next = M.empty}))], root = 0, last_i = 0}
+empty :: SuffixAutomation a
+empty = SuffixAutomation {states = M.fromList [(0,(SState {len = 0, link = Nothing, next = M.empty}))], root = 0, last_i = 0}
 
 
 linkedList :: Int -> SuffixAutomation a -> [(Int,SState a)]
-linkedList id aut = case link current of    Nothing -> [(id,current)]
-                                            Just lp -> (id,current):(linkedList lp aut)
-    where current = (states aut)M.!id
+linkedList cid aut = case link current of   Nothing -> [(cid,current)]
+                                            Just lp -> (cid,current):(linkedList lp aut)
+    where current = (states aut)M.!cid
 
 filter' :: (SState a -> Bool) -> [(Int,SState a)] -> [(Int,SState a)]
 filter' f xs = zip (map (fst) xs) $ filter f $ map (snd) xs
@@ -38,18 +45,24 @@ applyList :: [(Int,SState a)] -> SuffixAutomation a -> SuffixAutomation a
 applyList [] aut = aut
 applyList ((k,v):xs) aut = applyList xs $ SuffixAutomation { states = M.insert k v (states aut), root = (root aut), last_i = (last_i aut)}
 
-addItem :: (Ord a,Show a) => a -> SuffixAutomation a -> SuffixAutomation a
-addItem x aut = SuffixAutomation { states = (states aut), root = (root aut), last_i = id }
-                 where  st1 = map' (\a -> SState {   len = (len a)
+addItem :: (Ord a,Show a) => SuffixAutomation a -> a -> SuffixAutomation a
+addItem aut x = SuffixAutomation { states = result, root = (root aut), last_i = cid }
+                 where  patch = map' (\a -> SState {len = (len a)
                                                     ,link = (link a)
-                                                    ,next = M.insert x id (next a)}) $ path
-                        path = (takeWhile' (\a -> x `M.notMember` (next a)) (linkedList (last_i aut) aut))
-                        id = M.size (states aut)
+                                                    ,next = M.insert x cid (next a)}) $ path ++ [(cid,cur)] ++ clone_list
+                        path = takeWhile' (\a -> x `M.notMember` (next a)) (linkedList (last_i aut) aut)
+                        result = (states (applyList patch aut))
+                        cid = M.size (states aut)
                         cur = SState { len = (len ((states aut)M.!(last_i aut)) + 1), link = Just new_link, next = M.empty }
-                        new_link = case (link . snd . last $ path) of   Nothing ->  0
-                                                                        Just ip ->  if len p + 1 == len q
-                                                                                    then (next p)M.!x
-                                                                                    else fst clone
-                                                                                        where   clone = (M.size (states aut) + 1,SState { len = len p + 1, link = link q, next = next q})
-                                                                                                p = (states aut)M.!ip
-                                                                                                q = (states aut)M.!((next p)M.!x)
+                        ip = maybe (-1) (id) (link . snd . last $ path)
+                        p = (states aut) M.! ip
+                        q = (states aut) M.! ((next p)M.!x)
+                        clone = (M.size (states aut) + 1,SState { len = len p + 1, link = link q, next = next q})
+                        clone_list = if ip /= -1 && len p + 1 /= len q then [clone] else []
+                        new_link = if ip == -1  then    0
+                                                else    if len p + 1 == len q
+                                                            then (next p)M.!x
+                                                            else fst clone
+
+fromList :: (Ord a,Show a) => [a] -> SuffixAutomation a
+fromList xs = foldl (addItem) empty xs
